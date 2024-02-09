52b331a6179ad352399e40869efaad6391511c64
Giorgi chumburidze <159162080+GChumburidze31@users.noreply.github.com>
2024-02-09 08:16:11 -0500

    Update FibonacciSequence.java

--- src/FibonacciSequence.java
+++ src/FibonacciSequence.java
@@ -1,23 +1,18 @@
-import java.util.Scanner;
-/*
-Create a program that generates the Fibonacci sequence up to a number n provided by the user.
- */
 
-
-public class FibonacciSequence {
+public class FindMinValue {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        System.out.println("Enter your number, and we will generate a Fibonacci sequence based on it: ");
+        int[] list = {5, 10, 36, 2, 1};
+        int min = findMin(list);
+        System.out.println("The minimum value is: " + min);
+    }
 
-        int num = sc.nextInt();
-        int currentNum = 0;
-        int followNum = 1;
-        int newN = 0;
-        for (int i = 1; i != num; i++){
-            System.out.print(currentNum + " ");
-            newN = currentNum + followNum;
-            currentNum = followNum;
-            followNum = newN;
+    public static int findMin(int[] array) {
+        int min = array[0];
+        for (int num : array) {
+            if (min > num) {
+                min = num;
+            }
         }
+        return min;
     }
 }
